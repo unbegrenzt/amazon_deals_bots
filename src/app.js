9f0ab66182e5e68c86c3b4dc17c7bef79704aa5c
unbegrenzt <jmoralescenteno@gmail.com>
2020-09-27 13:35:31 -0600

    test heroku implementation

--- src/app.js
+++ src/app.js
@@ -33,10 +33,13 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+    response.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at http://localhost:" + app.get('port'))
 })
