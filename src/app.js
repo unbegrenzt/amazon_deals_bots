e2c70af58eef6908081a58a3ef7ab17b3b75d73c
unbegrenzt <jmoralescenteno@gmail.com>
2020-09-27 11:59:24 -0600

    express integration

--- src/app.js
+++ src/app.js
@@ -28,3 +28,15 @@ let job = new CronJob.CronJob(
     'Europe/Madrid'
 );
 job.start();
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
